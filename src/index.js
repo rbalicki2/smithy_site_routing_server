69c863346a76e4a0bbfabf5ec6f4e9747fff59ff
Robert Balicki <robert.balicki@gmail.com>
2019-03-23 23:24:22 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
+
+let currentJson = {};
+
+app.get('/', (req, res) => res.json(currentJson));
+
+app.post('/', (req, res) => {
+  currentJson = req.body;
+  res.status(200).end();
+});
+
+app.listen(port, () => console.log(`smithy_site_routing_server listening on port ${port}!`))
